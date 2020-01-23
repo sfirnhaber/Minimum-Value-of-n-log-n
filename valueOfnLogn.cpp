ad74bb3a652138279ade447bfbb9c682ac4bcb80
Sam Firnhaber <47701392+sfirnhaber@users.noreply.github.com>
2020-01-22 17:04:10 -0800

    Create valueOfnLogn.cpp

---
+++ valueOfnLogn.cpp
@@ -0,0 +1,18 @@
+  /*
+ * Minimum Value of n log n
+ * By Sam Firnhaber
+ */
+
+#include <iostream>
+#include <math.h>  
+
+int main() {
+    unsigned long long n = 1;
+    unsigned long long t;
+    std::cin >> t;
+    while (n * log2(n) < t)
+        n++;
+    n--;
+    std::cout << n << std::endl;
+    return 0;
+}
